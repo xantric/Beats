12a5cb5fdecb4cd287c37fb63e9072bc623ad7cb
IshanKamboj <ISHANKAMBOJ35@GMAIL.COM>
2021-04-28 14:45:12 +0530

    second commit

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 from pathlib import Path 
 import discord
 from discord.ext import commands
+import os
 class MusicBot(commands.Bot):
     def __init__(self):
         self._cogs=[p.stem for p in Path(".").glob("./bot/cogs/*.py")]
@@ -16,9 +17,9 @@ class MusicBot(commands.Bot):
     def run(self):
         self.setup()
 
-        with open("data/token.0","r", encoding="utf-8") as f:
-            TOKEN = f.read() 
-        
+##        with open("data/token.0","r", encoding="utf-8") as f:
+##            TOKEN = f.read() 
+        TOKEN = os.getenv("TOKEN")
         print("Running the bot")
         super().run(TOKEN,reconnect=True)
         
